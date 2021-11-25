0d59ee9b3a364a98bdb6c86e90dbaeca33611880
aotatsu8 <70871669+aotatsu8@users.noreply.github.com>
2021-11-25 16:07:31 +0900

    Initial commit

---
+++ src/router/Router.jsx
@@ -0,0 +1,50 @@
+import { Switch, Route } from "react-router-dom";
+import { Home } from "../Home";
+import { Page404 } from "../Page404";
+import { Page1Routes } from "./Page1Routes";
+import { Page2Routes } from "./Page2Routes";
+
+export const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        path="/page1"
+        render={({ match: { url } }) => (
+          <Switch>
+            {Page1Routes.map((router) => (
+              <Route
+                key={router.path}
+                exact={router.exact}
+                path={`${url}${router.path}`}
+              >
+                {router.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+      <Route
+        path="/page2"
+        render={({ match: { url } }) => (
+          <Switch>
+            {Page2Routes.map((router) => (
+              <Route
+                key={router.path}
+                exact={router.exact}
+                path={`${url}${router.path}`}
+              >
+                {router.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+      <Route path="*">
+        <Page404 />
+      </Route>
+    </Switch>
+  );
+};
